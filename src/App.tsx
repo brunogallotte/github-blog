a623ab45634234149859423bc4c6909b302af611
Bruno Gallotte <brunogallotte1@gmail.com>
2024-01-09 09:29:05 -0300

    Feat: add react-router-dom home page and global style with styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,16 @@
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
+
 export function App() {
-  return <h1>hello react</h1>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ThemeProvider>
+  )
 }
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,15 @@
+export const defaultTheme = {
+  blue: '#3294F8',
+  baseTitle: '#E7EDF4',
+  baseSubtitle: '#C4D4E3',
+  baseText: '#AFC2D4',
+
+  baseSpan: '#7B96B2',
+  baseLabel: '#3A536B',
+  baseBorder: '#1C2F41',
+  basePost: '#112131',
+
+  baseProfile: '#0B1B2B',
+  baseBackground: '#071422',
+  baseInput: '#040F1A',
+}
