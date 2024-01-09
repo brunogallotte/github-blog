e1a93b19731e7b3e6f34ee7a2e5d444d8c18d274
Bruno Gallotte <brunogallotte1@gmail.com>
2024-01-09 15:39:14 -0300

    Feat: add userinfo component

--- src/components/UserInfo/intex.tsx
+++ src/components/UserInfo/intex.tsx
@@ -1,20 +1,51 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faBuilding } from '@fortawesome/free-regular-svg-icons'
+import {
+  faUserGroup,
+  faArrowUpRightFromSquare,
+} from '@fortawesome/free-solid-svg-icons'
+import {
+  GitHubLink,
+  UserInfoContainer,
+  UserInfoContent,
+  UserInfoFooter,
+} from './styles'
+
 export function UserInfo() {
   return (
-    <div className="container">
-      <div>
-        <img
-          src="http://www.github.com/brunogallotte.png"
-          alt="Imagem de perfil"
-        />
-        <div>
-          <strong>Bruno Gallotte</strong>
-          <p>
-            Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu
-            viverra massa quam dignissim aenean malesuada suscipit. Nunc,
-            volutpat pulvinar vel mass.
-          </p>
-        </div>
-      </div>
-    </div>
+    <UserInfoContainer className="container">
+      <img
+        src="http://www.github.com/brunogallotte.png"
+        alt="Imagem de perfil"
+        className="profileImg"
+      />
+      <UserInfoContent>
+        <strong>Bruno Gallotte</strong>
+        <p>
+          Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu
+          viverra massa quam dignissim aenean malesuada suscipit. Nunc, volutpat
+          pulvinar vel mass.
+        </p>
+        <UserInfoFooter>
+          <div className="userInfoFooterItem">
+            <FontAwesomeIcon icon={faGithub} />
+            <span>brunogallotte</span>
+          </div>
+          <div className="userInfoFooterItem">
+            <FontAwesomeIcon icon={faBuilding} />
+            <span>RocketSeat</span>
+          </div>
+          <div className="userInfoFooterItem">
+            <FontAwesomeIcon icon={faUserGroup} />
+            <span>32 seguidores</span>
+          </div>
+        </UserInfoFooter>
+      </UserInfoContent>
+      <GitHubLink>
+        <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+        <span>github</span>
+      </GitHubLink>
+    </UserInfoContainer>
   )
 }
---
+++ src/components/UserInfo/styles.ts
@@ -0,0 +1,70 @@
+import styled from 'styled-components'
+
+export const UserInfoContainer = styled.div`
+  position: relative;
+  display: flex;
+  width: 100%;
+  background: ${(props) => props.theme.baseProfile};
+  padding: 2rem;
+  border-radius: 10px;
+  box-shadow: 0px 2px 28px 0px rgba(0, 0, 0, 0.2);
+  margin-top: -6rem;
+  gap: 2rem;
+
+  .profileImg {
+    width: 148px;
+    height: 148px;
+    object-fit: cover;
+    border-radius: 8px;
+  }
+`
+
+export const UserInfoContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+
+  strong {
+    font-size: 1.5rem;
+    color: ${(props) => props.theme.baseTitle};
+  }
+
+  p {
+    color: ${(props) => props.theme.baseText};
+    line-height: 160%;
+  }
+
+  footer {
+    margin-top: auto;
+  }
+`
+
+export const UserInfoFooter = styled.footer`
+  display: flex;
+  color: ${(props) => props.theme.baseSubtitle};
+  gap: 1.5rem;
+
+  .userInfoFooterItem {
+    display: flex;
+    align-items: center;
+
+    svg {
+      width: 1.125rem;
+      height: 1.125rem;
+      color: ${(props) => props.theme.baseLabel};
+      margin-right: 0.5rem;
+    }
+  }
+`
+
+export const GitHubLink = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  margin-top: 40px;
+  margin-right: 2rem;
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  color: ${(props) => props.theme.blue};
+`
